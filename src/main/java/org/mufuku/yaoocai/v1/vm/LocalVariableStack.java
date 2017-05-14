fe8b2aab071b9faa18800cf302453449a51e2304
Andreas Etzlstorfer <a.etzlstorfer@gmail.com>
2017-05-14 19:39:47 +0200

    #2 while support: Added while support with unit test

--- src/main/java/org/mufuku/yaoocai/v1/vm/LocalVariableStack.java
+++ src/main/java/org/mufuku/yaoocai/v1/vm/LocalVariableStack.java
@@ -1,17 +1,17 @@
 package org.mufuku.yaoocai.v1.vm;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Andreas Etzlstorfer (a.etzlstorfer@gmail.com)
  */
 public class LocalVariableStack {
 
-    private final List<Object> variables = new ArrayList<>();
+    private final Map<Short, Object> variables = new HashMap<>();
 
     public void setValue(short index, Object value) {
-        variables.add(index, value);
+        variables.put(index, value);
     }
 
     public Object getValue(short index) {
